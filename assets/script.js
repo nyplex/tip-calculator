91492f9d3f8dfd979b8c708f6f1253e997b7c1ee
Alex <nypels.alexandre@outlook.com>
2021-11-25 21:20:45 +0000

    cleaning up functions

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,55 @@
 //Declare all the variables
-let percentageTips;
+let percentageTips
+let errors = [];
+const tips_button = document.querySelectorAll("#tips-wrapper > button")
+const calculate_button = document.getElementById("calcul-btn")
+const tip_manual_input = document.getElementById("tip-manual")
 
+let isNumber = (number) => (typeof number !== 'number') || (number !== Number(number)) ? false : true
 
-let isNumber = (number) => {
-    return (typeof number !== 'number') || (number !== Number(number)) ? false : true
-}
-
-let validateInput = (value) => {
-    return isNumber(value) && value != undefined && value != 0 ? true : false
-}
+let validateInput = (value) => isNumber(value) && value != undefined && value != 0 ? true : false
 
 let getInputValue = (id) => {
     let value = parseInt(document.getElementById(id).value)
-    return validateInput(value) ? value : false 
+    if(validateInput(value) === false) errors.push(id)
+    else return true
 }
 
-document.getElementById("calcul-btn").addEventListener('click', () => {
-    console.log(getInputValue("total-bill"))
-    console.log(getInputValue("total-people"));
-})
+let removeTipClass = () => document.querySelectorAll('.tip-button').forEach((e) => e.classList.remove('tip-selected'));
 
+let changeClassTips = (id) => {
+    removeTipClass()
+    document.getElementById(id).classList.add("tip-selected")
+}
 
-let tips_button = document.querySelectorAll("#tips-wrapper > button");
+let getTipNumber = (id) => getInputValue(id)
+
+let displayErrors = (e) => {
+    e.forEach((error) => {
+        let label = document.querySelector('label[for='+error+']')
+        label.style.color = 'red'
+        label.innerHTML = "Something wrong"
+    })
+}
+
+tip_manual_input.addEventListener('click', (e) => removeTipClass())
+
 tips_button.forEach(
     function(currentValue, currentIndex, listObj) {
       currentValue.addEventListener('click', (e) => {
-        percentageTips = currentValue.value
+        changeClassTips(currentValue.id)
+        percentageTips = getTipNumber(currentValue.id)
       })
     }
 );
+
+calculate_button.addEventListener('click', () => {
+    let bill = getInputValue("total-bill")
+    let people = getInputValue("total-people")
+    if(errors.length > 0) displayErrors(errors);
+})
+
+
+
+
 
